ba9a4c5bdd494bb49fadee96991a72c89e964074
Vutaliy <javavetal@list.ru>
2023-05-30 20:03:13 +0300

    first commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,44 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    @Bean(name = "cat")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
+    }
+
+    @Bean(name = "dog")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        return dog;
+    }
+
+    @Bean
+    public Timer getT() {
+        Timer timer = new Timer();
+        return timer;
+    }
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    @Qualifier("cat")
+    private Animal animal1;
+
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +46,10 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+
+        System.out.println("Say:");
+        System.out.println(animal1.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
     }
 }
