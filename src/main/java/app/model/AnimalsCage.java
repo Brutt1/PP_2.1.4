3bfb45c8560d5a66b188b75f42cfcfcee81bbec0
Vutaliy <javavetal@list.ru>
2023-09-26 12:32:01 +0300

    убрал animal1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -2,38 +2,17 @@ package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-    @Bean(name = "cat")
-    public Cat getCat() {
-        Cat cat = new Cat();
-        return cat;
-    }
-
-    @Bean(name = "dog")
-    public Dog getDog() {
-        Dog dog = new Dog();
-        return dog;
-    }
-
-    @Bean
-    public Timer getT() {
-        Timer timer = new Timer();
-        return timer;
-    }
 
     @Autowired
     @Qualifier("dog")
     private Animal animal;
 
     @Autowired
-    @Qualifier("cat")
-    private Animal animal1;
-
-    @Autowired
+    @Qualifier("timer")
     private Timer timer;
 
     public Timer getTimer() {
@@ -44,12 +23,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
-
-        System.out.println("Say:");
-        System.out.println(animal1.toString());
-        System.out.println("At:");
-        System.out.println(new Timer().getTime());
     }
 }
